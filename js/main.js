f48a8097fbc23c75c42c4e170caed5485f3b80de
Wooo <mauricio@wooo.cl>
2017-01-16 19:36:58 -0300

    Sistema de templates básico

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+// templates
+
+window.template = function(templateName, context) {
+  var html = $('#' + templateName).html();
+
+  for (var p in context) {
+    if (!context.hasOwnProperty(p)) {
+      continue;
+    }
+    html = html.replace(new RegExp(':' + p, 'g'), context[p]);
+  }
+
+  return $(html);
+};
+
+
 //mapa
 $(document).ready(function(){
 	var mapOptions = {
