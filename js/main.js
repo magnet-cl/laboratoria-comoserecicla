b001dc8c618e3889c0fb6775adca4c862d7a16ac
marcia <marcia.millanao@gmail.com>
2017-01-16 13:37:12 -0300

    html mapa

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//mapa
+$(document).ready(function(){
+	var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(-33.437411, -70.650146),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  		}
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+});
+//mapa
