b3260f6e42d71f3f34f5d5386b09bcb922128a8a
marcia <marcia.millanao@gmail.com>
2017-01-16 21:56:17 -0300

    agrego iconos a html agregar

--- js/search.js
+++ js/search.js
@@ -1,47 +1,37 @@
 $(document).ready(function(){
-var products = "";
-var url = "";
+	var products = "";
+	var url = "";
 
-var dibujaProd
+	var dibujaProd
 
-$('#search').keyup(function(event) {
-	/* Act on the event */
-	$.ajax({
+	$('#search').keyup(function(event) {
+		/* Act on the event */
+		$.ajax({
 			url: 'http://10.77.3.77:3000/search.json',
 			data: {q: 'botella'},
 			success: function(products) {
-	   		console.log(products);
-	   		products.forEach(function(product){
-	   			$("#recent").html('');
-	   			$("#recent").append(template("product-template", product));
-	   			$("#suggest").addClass('hidden');
-	   		});
+				console.log(products);
+				products.forEach(function(product){
+					$("#recent").html('');
+					$("#recent").append(template("product-template", product));
+					$("#suggest").addClass('hidden');
+				});
 			}
 		})
-});
-		
-
-
-
-
-
-
-
-
-
 	});
+});
 
 window.template = function(templateName, context) {
-  var html = $('#' + templateName).html();
+	var html = $('#' + templateName).html();
 
-  for (var p in context) {
-    if (!context.hasOwnProperty(p)) {
-      continue;
-    }
-    html = html.replace(new RegExp(':' + p, 'g'), context[p]);
-  }
+	for (var p in context) {
+		if (!context.hasOwnProperty(p)) {
+			continue;
+		}
+		html = html.replace(new RegExp(':' + p, 'g'), context[p]);
+	}
 
-  return $(html);
+	return $(html);
 };
 
 
